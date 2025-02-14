2b122690b41fb92ce4402b1920fe074341158c04
mariobet23440 <bet23440@uvg.edu.gt>
2025-02-14 13:30:12 -0600

    Cambios y simplificación
    Hice el cálculo de la configuración del reloj en modo normal para evitar usar el registro COUNTER del ejemplo de Pedro. Ahora cada activación de la bandera TOV0 ocurre cada 100 ms

--- Lab2_Timer_Counter0/Lab2_Timer_Counter0/main.asm
+++ Lab2_Timer_Counter0/Lab2_Timer_Counter0/main.asm
@@ -6,11 +6,23 @@
 ;
 
 /*
-PRESCALER NECESARIO
-t = 100 ms
-f = 16 MHz
-T = 0.0000000625 (Periodo con frecuencia original)
+CONFIGUACIÓN DE TIMER0
+Estamos usando TIMER0 como un temporizador en modo NORMAL. 
+t_delay = 100 ms
+f_clk = 1 MHz
+n = 8 (Recordemos que es el número de bits del registro del *contador*)
 
+Prescaler >= (t_delay * f_clk) / 2**n = (0.1*10E6)(2POW(8)) = 390
+Prescaler >= 390
+Escogemos un prescaler de 1024
+
+Calculamos el tiempo máximo que puede contar TIMER0
+Tmax = (2**n * Prescaler) / f_clk = (2**8 * 1024) / 10E6 = 0.262s = 262 ms 
+
+Determinamos el valor inicial de TIMER0
+TCNT0 = 256 - (f_clk * t_deseado) / Prescaler = 256 - (10E6 * 0.100) / 1024 = 158
+
+Con este cálculo el contador alcanza 100 ms directamente por cada conteo realizado.
 */
 
 
@@ -18,7 +30,12 @@ T = 0.0000000625 (Periodo con frecuencia original)
 .include "M328PDEF.inc"
 .cseg
 .org    0x0000
-.def	COUNTER = R20
+
+// Definiciones
+.equ PRESCALER = (1<<CS01) | (1<<CS00)
+.equ TIMER_START = 158                   ; Valor inicial del Timer0
+.equ OVERFLOWS = 10                      ; Cantidad de desbordamientos para 100 ms
+.def COUNTER = R20						 ; REGISTRO A MOSTRAR EN PUERTOS
 
 // Lookup Table para Display de 7 Segmentos
 .equ SEVENSD1 =	0b0010_0001
@@ -50,14 +67,35 @@ SETUP:
     LDI     R16, 0xF0	// Activar Pull-ups en entradas y desactivar salidas inicialmente
     OUT     PORTC, R16
 
-    // Registros de contadores
-	CLR     R16
-	CLR		R17
+	// Configurar Prescaler Principal
+	LDI		R16, (1 << CLKPCE)
+	STS     CLKPR, R16          // Habilitar cambio de PRESCALER
+    LDI     R16, 0x04
+    STS     CLKPR, R16          // Configurar Prescaler a 1 Mhz
 
-MAINLOOP:
+	// Inicializar timer0
+    CALL    INIT_TMR0
 
 
-DELAY:
-	CLR		R17
+MAIN_LOOP:
+    // Revisión de la bandera de Overflow en TIMER0
+	IN      R16, TIFR0          // Leer registro de interrupción de TIMER0
+    SBRS    R16, TOV0           // Salta si el bit 0 está "set" (TOV0 bit, Bandera de Overflow)
+    RJMP    MAIN_LOOP           // Reiniciar loop
+	
+	// Si el reloj alcanza un desborde, se ejecuta el siguiente fragmento de código
+	SBI     TIFR0, TOV0         // Limpiar bandera de "overflow" (No se limpia automáticamente)
+	LDI     R16, TIMER_START    // Establecer el valor inicial del contador TCNT0
+    OUT     TCNT0, R16          // Volver a cargar valor inicial en TCNT0 
+    RJMP    MAIN_LOOP			// Regresar a Mainloop
+
+
+// Inicializar Timer0
+INIT_TMR0:
+    LDI     R16, PRESCALER				// Configurar un registro para setear las posiciones de CS01 y CS00
+    OUT     TCCR0B, R16					// Setear prescaler del TIMER0 a 64 (CS01 = 1 y CS00 = 0)
+    LDI     R16, TIMER_START			// Empezar el conteo con un valor de 100
+    OUT     TCNT0, R16					// Cargar valor inicial en TCNT0
+    RET
 
 		
