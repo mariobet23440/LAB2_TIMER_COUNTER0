9d00459a317db8895d4bcca6b00ad0c5cf1753b8
mariobet23440 <bet23440@uvg.edu.gt>
2025-02-14 17:43:31 -0600

    ENTREGA DE LAB
    Cambio de prescaler principal a 128 y delay por TIMER0 a 20 ms. Adición de entradas en PORTB

--- Prelab2/Prelab2/Prelab2/main.asm
+++ Prelab2/Prelab2/Prelab2/main.asm
@@ -4,36 +4,24 @@
 ; Created: 14/02/2025 00:07:08
 ; Author : mario
 ; Link a Github: https://github.com/mariobet23440/LAB2_TIMER_COUNTER0
+; ENTREGA DE PRELAB: 14/02/2025 - 14:01
 
 /*
-- HISTORIAL DE VERSIONES -
-
-ENTREGA DE PRELAB: 14/02/2025 - 14:01
-- Implementación del contador con TIMER0.
-- Muestra el resultado del contador en los cuatro LEDs y en el display de 7 segmentos.
-
-ENTREGA DE LAB: 14/02/2024 - 18:55
-- Implementación de contador con entradas
-*/
-
-
-/*
-CONFIGUACIÓN DE TIMER0 (Es diferente respecto al PRELAB)
-Vamos a disminuir el tiempo del delay a 20 ms (El tiempo que tarda un botón en dejar de rebotar)
+CONFIGUACIÓN DE TIMER0
 Estamos usando TIMER0 como un temporizador en modo NORMAL. 
-t_delay = 20 ms
+t_delay = 100 ms
 f_clk = 1 MHz
 n = 8 (Recordemos que es el número de bits del registro del *contador*)
 
-Prescaler >= (t_delay * f_clk) / 2**n = (0.02*10E6)(2POW(8)) = 78.125
-Prescaler >= 78
-Escogemos un prescaler de 128 (0111)
+Prescaler >= (t_delay * f_clk) / 2**n = (0.1*10E6)(2POW(8)) = 390
+Prescaler >= 390
+Escogemos un prescaler de 1024
 
 Calculamos el tiempo máximo que puede contar TIMER0
-Tmax = (2**n * Prescaler) / f_clk = (2**8 * 1024) / 10E6 = 0.03s = 32 ms 
+Tmax = (2**n * Prescaler) / f_clk = (2**8 * 1024) / 10E6 = 0.262s = 262 ms 
 
 Determinamos el valor inicial de TIMER0
-TCNT0 = 256 - (f_clk * t_deseado) / Prescaler = 256 - (10E6 * 0.02) / 128 = 156
+TCNT0 = 256 - (f_clk * t_deseado) / Prescaler = 256 - (10E6 * 0.100) / 1024 = 158
 
 Con este cálculo el contador alcanza 100 ms directamente por cada conteo realizado.
 */
@@ -45,11 +33,11 @@ Con este cálculo el contador alcanza 100 ms directamente por cada conteo realiza
 .org    0x0000
 
 // Definiciones
-.equ PRESCALER = (1<<CS02) |(1<<CS01) | (1<<CS00)	; Prescaler de TIMER0 (En este caso debe ser de 128)
-.equ TIMER_START = 158								; Valor inicial del Timer0
-.equ OVERFLOWS = 10									; Cantidad de desbordamientos para 100 ms
-.def COUNTER_PORT = R20								; REGISTRO A MOSTRAR EN PUERTOS
-.def SEVENSD_OUT = R21								; Registro temporal
+.equ PRESCALER = (1<<CS02) | (1<<CS00)	 ; Prescaler de TIMER0 (En este caso debe ser de 1024)
+.equ TIMER_START = 158                   ; Valor inicial del Timer0
+.equ OVERFLOWS = 10                      ; Cantidad de desbordamientos para 100 ms
+.def COUNTER_PORT = R20					 ; REGISTRO A MOSTRAR EN PUERTOS
+.def SEVENSD_OUT = R21					 ; Registro temporal
 
 // Lookup Table para Display de 7 Segmentos
 .equ SEVENSD0 =	0b0111_0111
@@ -76,15 +64,8 @@ LDI     R16, HIGH(RAMEND)
 OUT     SPH, R16
 
 SETUP:
-	// Activación de pines de entrada en el puerto B
-    LDI     R16, 0x00
-    OUT     DDRB, R16
-    LDI     R16, 0xFF	// Desactivar salidas inicialmente
-    OUT     PORTB, R16	// Por alguna razón no funciona si usamos algunos bits de PORTC como entradas y otros como salidas
-	// Sin embargo, usar el puerto B solo para recibir entradas fue una solución rápida que terminó funcionando.
-	
-	// Activación de pines de salida en el puerto C
-    LDI     R16, 0xFF	// Primeros cuatro bits como salidas y los primeros dos bits como entradas
+	// Activación de pines de entrada y salida en el puerto C
+    LDI     R16, 0x0F	// Primeros cuatro bits como salidas y los primeros dos bits como entradas
     OUT     DDRC, R16
     LDI     R16, 0xF0	// Activar Pull-ups en entradas y desactivar salidas inicialmente
     OUT     PORTC, R16
@@ -120,23 +101,9 @@ MAIN_LOOP:
 	LDI     R16, TIMER_START    // Establecer el valor inicial del contador TCNT0
     OUT     TCNT0, R16          // Volver a cargar valor inicial en TCNT0 
 
-	// LAB - Incrementar y decrementar el contador con dos botones
-	IN		R17, PINB				// Leer el valor de PINC
-	SBRS	R17, PB0
-	INC		COUNTER_PORT
-	SBRS	R17, PB1
-	DEC		COUNTER_PORT			// ¡Y listo! Eso era todo el lab xd
-	
-	/*
-	¿Por qué esto ya tiene antirrebote?
-	Observe que este segmento de código se ejecuta únicamente cuando la bandera TOV0 está activa.
-	Por lo tanto el TIMER0 cuenta hasta que se active la bandera y hasta ese entonces el valor de los bits puede cambiar.
-	No es necesario revisar dos veces y hacer un delay de por medio.
-	*/
-
-	
 	// PRELAB - Mostrar en LEDs
-	// Mostrar Contador en PORTC
+	// Contador en PORTC
+	INC		COUNTER_PORT			// Incrementar el valor de COUNTER_PORTC
 	ANDI	COUNTER_PORT, 0X0F		// Truncar el valor obtenido a 4 bits
 	OUT		PORTC, COUNTER_PORT	// Mostrar el resultado en PORTC
 
